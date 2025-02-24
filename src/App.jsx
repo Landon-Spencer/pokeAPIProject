5174d96bc4e32e37a5c0511c1d7b5d8795b4c81b
Landon-Spencer <landon.r.spencer@gmail.com>
2025-02-24 09:39:59 -0700

    able to console log pokemon

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,12 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import MainDisplay from './MainDisplay'
 
 function App() {
   const [count, setCount] = useState(0)
+
+  // MainDisplay();
 
   return (
     <>
@@ -28,6 +31,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <MainDisplay/>
     </>
   )
 }
